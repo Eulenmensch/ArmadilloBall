a56dd646dd7da1efc6d1fb14db28c328aa0f933f
Yoshua Woo <yoshua.woo@gmail.com>
2020-07-11 18:02:49 +0200

    Move Direction on Input end; Input direction Arrow;

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,39 +8,73 @@ public class MovementController : MonoBehaviour
     [SerializeField] private CommandInvoker commandInvoker = null;
     [SerializeField] private EnergyController energyController = null;
     [SerializeField] private FloatVariable energy = null;
+    [SerializeField] private GameObject arrowPrefab = null;
 
     private float timePressed;
     private Vector2 direction;
+    private GameObject arrow;
+    private MoveArrow arrowController;
 
 
-    public void OnMove(InputValue value)
+    // public void OnMove(InputValue value)
+    // {
+    //     if (energy.value <= 0) return;
+    //     if (timePressed == 0)
+    //     {
+    //         direction = value.Get<Vector2>();
+    //         timePressed = Time.time;
+    //         energyController.StartEnergyDrain();
+    //     }
+    //     else
+    //     {
+    //         energyController.StopEnergyDrain();
+    //         SendMoveCommand();
+    //     }
+    // }
+
+    public void OnMove(InputAction.CallbackContext context)
     {
         if (energy.value <= 0) return;
-
-        if(timePressed == 0)
+        if (context.started)
         {
-            direction = value.Get<Vector2>();
             timePressed = Time.time;
             energyController.StartEnergyDrain();
+            ShowMoveArrow();
         }
-        else
+        if (context.performed)
+        {
+            direction = context.ReadValue<Vector2>();
+            if (direction == Vector2.zero)
+            {
+                print("input is zero");
+            }
+            MoveAndScaleMoveArrow();
+        }
+        if (context.canceled)
         {
             energyController.StopEnergyDrain();
             SendMoveCommand();
+            HideMoveArrow();
         }
     }
 
-    public void OnCurl()
+    public void OnCurl(InputAction.CallbackContext context)
     {
-        if (energy.value < energyController.EnergyCostCurleToggle) return;
+        if (context.canceled)
+        {
+            if (energy.value < energyController.EnergyCostCurleToggle) return;
 
-        energy.value -= energyController.EnergyCostCurleToggle;
-        commandInvoker.AddCommand(new CurlCommand());
+            energy.value -= energyController.EnergyCostCurleToggle;
+            commandInvoker.AddCommand(new CurlCommand());
+        }
     }
 
-    public void OnSubmitMovement()
+    public void OnSubmitMovement(InputAction.CallbackContext context)
     {
-        StartCoroutine(commandInvoker.ExectueAllCommands());
+        if (context.canceled)
+        {
+            StartCoroutine(commandInvoker.ExectueAllCommands());
+        }
     }
 
     public void SendMoveCommand()
@@ -48,5 +82,23 @@ public class MovementController : MonoBehaviour
         float pressDuration = Time.time - timePressed;
         commandInvoker.AddCommand(new MoveCommand(pressDuration, direction));
         timePressed = 0f;
+    }
+
+    private void ShowMoveArrow()
+    {
+        arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+        arrowController = arrow.GetComponent<MoveArrow>();
+    }
+
+    private void MoveAndScaleMoveArrow()
+    {
+        Vector3 arrowDirection = new Vector3(direction.x, 0, direction.y);
+        arrowController.SetArrowDirection(arrowDirection);
+        arrowController.SetArrowLength(Time.time - timePressed);
+    }
+
+    private void HideMoveArrow()
+    {
+        Destroy(arrow);
     }
 }
---
+++ Assets/Scripts/UI/MoveArrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArrow : MonoBehaviour
+{
+    [SerializeField] GameObject Tip;
+    [SerializeField] GameObject Body;
+    [SerializeField] float LengthMultiplier;
+
+    public void SetArrowLength(float _length)
+    {
+        var scale = Body.transform.localScale;
+        Body.transform.localScale = new Vector3(scale.x, _length * LengthMultiplier, scale.z);
+        Tip.transform.localPosition = new Vector3(0, 0.05f, Body.transform.localScale.y * 0.55f);
+    }
+    public void SetArrowDirection(Vector3 _direction)
+    {
+        transform.rotation = Quaternion.LookRotation(_direction, Vector3.up);
+    }
+}
