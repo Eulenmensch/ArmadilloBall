b2f65bc3e33cdb86161a0007a0a622c72ea4a231
Yannick Pfeiffer <yannick_pfeiffer@yahoo.com>
2020-07-11 03:16:28 +0200

    Initial PlayerInput Setup

---
+++ Assets/PlayerActions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerActions : MonoBehaviour
+{
+    public void OnMove()
+    {
+        Debug.Log("Test");
+    }
+}
