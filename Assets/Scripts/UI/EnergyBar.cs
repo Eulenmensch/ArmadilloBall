461038f7a3bf8c89f200d4b4c8015762c15b683b
Yoshua Woo <yoshua.woo@gmail.com>
2020-07-12 16:21:14 +0200

    Camera Collider; EnergyBar Drain;

---
+++ Assets/Scripts/UI/EnergyBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    [SerializeField] FloatVariable energy;
+
+    private float maxEnergy;
+    private Image image;
+
+    private void Start()
+    {
+        maxEnergy = energy.value;
+        image = GetComponent<Image>();
+    }
+    void Update()
+    {
+        image.fillAmount = energy.value / maxEnergy;
+    }
+}
