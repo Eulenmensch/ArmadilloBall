362b2450cf14fb29c2d3001751799e8f4ff8ab89
Yoshua Woo <yoshua.woo@gmail.com>
2020-07-12 20:23:51 +0200

    Dance Anim; prefab stuff;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
@@ -92,9 +92,9 @@ public class Player : MonoBehaviour
 
     public bool IsFalling()
     {
-        foreach(var contactPoints in collisions)
+        foreach (var contactPoints in collisions)
         {
-            foreach(ContactPoint contactPoint in contactPoints.Value)
+            foreach (ContactPoint contactPoint in contactPoints.Value)
             {
                 if (transform.position.y > contactPoint.point.y)
                     return false;
@@ -111,6 +111,20 @@ public class Player : MonoBehaviour
             {
                 if (transform.position.y > contactPoint.point.y)
                     return contactPoint.normal;
+            }
+        }
+
+        return Vector3.zero;
+    }
+
+    public Vector3 GetGroundPosition()
+    {
+        foreach (var contactPoints in collisions)
+        {
+            foreach (ContactPoint contactPoint in contactPoints.Value)
+            {
+                if (transform.position.y > contactPoint.point.y)
+                    return contactPoint.point;
             }
         }
 
