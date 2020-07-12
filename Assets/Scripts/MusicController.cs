15a21a2df7edf33d45122cee068d1a939b52a25a
lukas-mayer <lukasmmayer@gmail.com>
2020-07-12 03:54:28 +0200

    Fmod dies das

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        joo = FMODUnity.RuntimeManager.CreateInstance("event:/Musik/Phase 1");
+        joo.setParameterByName("egitarre an", 1);
+        joo.setParameterByName("horns an", 0);
+        joo.start();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public FMOD.Studio.EventInstance joo;
+}
